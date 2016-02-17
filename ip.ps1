3baf90a17e92bf602c305138250da48ce5bb637a
auzzie32 <austin@auzzie32.com>
2016-02-16 19:32:16 -0600

    Directory is interactive!
    Selecting a directory is now done in the shell, interactively using
    Read-Host

--- ip.ps1
+++ ip.ps1
@@ -5,9 +5,11 @@ $s = 60                 # The time in seconds between loops
 $minRaw = ($t * ($s + '.01') / 60)
 $minRound = [math]::Round($minRaw,2)
 "This script will run for approximatly $minRound minutes"
+$dir = Read-Host 'Where would you like the file to be saved? Default is Desktop'
+if ($dir -eq "") {$dir = "Desktop/ip-history"}
 For (; $t -gt 0 ; $t--) {$ip = Invoke-WebRequest -Uri https://api.ipify.org?format=json | ConvertFrom-Json
 $a = Get-Date
-Add-Content Desktop/ip-history.txt "$a $ip"
+Add-Content "$dir" "$a $ip"
 $line++
 "$line records written"
 Start-Sleep -s $s
